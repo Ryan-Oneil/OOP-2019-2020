4f20f8fed6018eba363fc12cf0a48744d917dd92
Ryan-Oneil <44525616+Ryan-Oneil@users.noreply.github.com>
2020-02-05 16:41:48 +0000

    Test

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,9 +1,7 @@
 package ie.tudublin;
 
-public class Main
-{
-    public static void main(String[] arg)
-    {
+public class Main {
+    public static void main(String[] arg) {
         System.out.println("Hello world!");
 
         Animal misty = new Dog(); // Polymorphism
