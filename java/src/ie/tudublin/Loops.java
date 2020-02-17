ec93042961d3de2983f108e3718f1fcbfed14fca
Ryan-Oneil <44525616+Ryan-Oneil@users.noreply.github.com>
2020-02-17 11:40:09 +0000

    test

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -2,49 +2,94 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class Loops extends PApplet
-{	
+public class Loops extends PApplet {
+
+	private int sizeX = 500;
+	private int sizeY = 500;
+	private int centre = 250;
+
 	public void settings()
 	{
-		size(500, 500);
+		size(sizeX, sizeY);
 	}
 
-	public void setup() 
-	{
-		
+	public void setup() {
+//		colorMode(HSB);
+//		drawThirdPattern();
+		//drawRainbowCircle();
+		drawGrid();
 	}
 
-	
-	public void keyPressed()
-	{
-		if (key == ' ')
-		{
-			
+	public void draw() {
+		//drawFirstPattern();
+		//drawCircle();
+		//drawThirdPattern();
+		//drawGrid();
+	}
+
+	private void drawThirdPattern() {
+		int colorSelection = 0;
+
+		for (int i = 0; i <500; i+=50) {
+			fillNextColor(colorSelection++);
+			rect(i, 0, 50, sizeY);
 		}
-	}	
+	}
 
+	private void drawRainbowCircle() {
+		int colorSelection = 0;
 
-	public void draw()
-	{	
-		background(0);		
-		stroke(255);
-		line(10, 10, 100, 10);
-		line(10, 20, 100, 20);
-		line(10, 30, 100, 30);
-		line(10, 40, 100, 40);
-
-		for(int y = 10 ; y <= 40 ; y += 10)
-		{
-			line(200, y, 300, y);
+		for (int i = 25; i <500; i+=50) {
+			fillNextColor(colorSelection++);
+			circle(i, centre, 50);
 		}
+	}
 
-		int yy = 10;
-		while(yy <= 40)
-		{
-			line(300, yy, 400, yy);
-			yy += 10;
+	private void drawCircle() {
+		for (int i = 25; i < 500; i+= 50) {
+			circle(i, centre, 50);
 		}
-		
-	
+	}
+
+	public void drawFirstPattern() {
+		for (int i = 0; i < 520; i+= 20) {
+			line(centre, centre, i, 0);
+			line(centre, centre, 0, i);
+
+			line(centre, centre, 500, i);
+			line(centre, centre, i, 500);
+		}
+	}
+
+	public void drawGrid() {
+		background(0);
+		float gap = width * 0.1f;
+		float textGap = gap / 2f;
+		stroke(0, 255, 0);
+
+		for (int i = -5; i <= 5; i++) {
+			float x = map(i, -5, 5, gap, height - gap);
+
+			text(i, x, textGap);
+			line(x , gap, x, height - gap);
+
+			text(i, textGap, x);
+			line(gap, x, width - gap, x);
+		}
+	}
+
+	private void fillNextColor(int colorSelection) {
+		int[][] colors = {{255, 0, 0},
+			{255, 105, 0},
+			{255, 225, 53},
+			{208, 255, 20},
+			{106, 255, 77},
+			{0, 255, 0},
+			{152, 255, 152},
+			{0, 255, 255},
+			{102, 179, 255},
+			{0, 0, 255}};
+
+		fill(colors[colorSelection][0], colors[colorSelection][1], colors[colorSelection][2]);
 	}
 }
